f08c3fe369f28d11aea84e8b2f5f768e14c0729a
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 13:22:22 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -16,21 +16,19 @@ end
 
 
  def apply_coupons(cart, coupons)
-   cart.each do |food, info|
-     coupons.each do |coupon|
-       if cart.keys.include?(coupon[:item])
-         if food == coupon[:item] && cart[coupon[:item]][:count] >= coupon[:num]
-           itemwithCoupon = "#{coupon[:item]} W/COUPON"
-           if cart[itemwithCoupon]
-             cart[itemwithCoupon][:count] += coupon[:num]
-             cart[coupon[:item]][:count] -= coupon[:num]
-           else
-             cart[itemwithCoupon] = {}
-             cart[itemwithCoupon][:price] = (coupon[:cost] / coupon[:num])
-             cart[itemwithCoupon][:clearance] = cart[coupon[:item]][:clearance]
-             cart[itemwithCoupon][:count] = coupon[:num]
-             cart[coupon[:item]][:count] -= coupon[:num]
-           end
+  coupons.each do |coupon|
+    if cart.keys.include?(coupon[:item])
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        itemwithCoupon = "#{coupon[:item]} W/COUPON"
+        if cart[itemwithCoupon]
+          cart[itemwithCoupon][:count] += coupon[:num]
+          cart[coupon[:item]][:count] -= coupon[:num]
+        else
+          cart[itemwithCoupon] = {}
+          cart[itemwithCoupon][:price] = (coupon[:cost] / coupon[:num])
+          cart[itemwithCoupon][:clearance] = cart[coupon[:item]][:clearance]
+          cart[itemwithCoupon][:count] = coupon[:num]
+          cart[coupon[:item]][:count] -= coupon[:num]
         end
       end
       if cart[coupon[:item]][:count] >= coupon[:num]
@@ -39,8 +37,17 @@ end
     end
   end
   cart
+
 end
 
+#cart is a hash with 2 keys with a nested hash with 3 values
+  #coupons is an array with a hash with 3 keys and 3 values
+  #if coupons[0].values.include?(cart.keys[0]) will find avocado in coupon
+  #coupons[0].values[0] is AVOCADO in coupon
+  #cart.keys[0] is AVOCADO in cart
+  #coupons[0].values[0] + " W/COUPON" creates "AVOCADO W/COUPON"
+  #coupons[0].values[1] = 2 the number of coupons
+  #coupons[0].values[2] = 5.0 which is the number off need to be divded by 2(number of coupons
 
 def apply_clearance(cart)
   # code here
