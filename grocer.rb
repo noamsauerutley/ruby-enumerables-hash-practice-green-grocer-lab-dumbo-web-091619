5cea4edba52abef600f99d3c84979b3dc31c7e53
noam sauer-utley <noamsauerutley@gmail.com>
2019-08-28 13:20:52 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -14,53 +14,31 @@ def consolidate_cart(cart)
   cart_hash
 end
 
-#
-#  def apply_coupons(cart, coupons)
-#    cart.each do |food, info|
-#      coupons.each do |coupon|
-#        if cart.keys.include?(coupon[:item])
-#          if food == coupon[:item] && cart[coupon[:item]][:count] >= coupon[:num]
-#            itemwithCoupon = "#{coupon[:item]} W/COUPON"
-#            if cart[itemwithCoupon]
-#              cart[itemwithCoupon][:count] += coupon[:num]
-#              cart[coupon[:item]][:count] -= coupon[:num]
-#            else
-#              cart[itemwithCoupon] = {}
-#              cart[itemwithCoupon][:price] = (coupon[:cost] / coupon[:num])
-#              cart[itemwithCoupon][:clearance] = cart[coupon[:item]][:clearance]
-#              cart[itemwithCoupon][:count] = coupon[:num]
-#              cart[coupon[:item]][:count] -= coupon[:num]
-#            end
-#         end
-#       end
-#       if cart[coupon[:item]][:count] >= coupon[:num]
-#         apply_coupons
-#       end
-#     end
-#   end
-#   cart
-# end
-
-
 
  def apply_coupons(cart, coupons)
-result = {}
-  # code here#
-  cart.each do |food, info|
-    coupons.each do |coupon|
-      if food == coupon[:item] && info[:count] >= coupon[:num]
-        info[:count] =  info[:count] - coupon[:num]
-        if result["#{food} W/COUPON"]
-          result["#{food} W/COUPON"][:count] += 1
-        else
-          result["#{food} W/COUPON"] = {:price => coupon[:cost], :clearance => info[:clearance], :count => 1}
+     coupons.each do |coupon|
+       if cart.keys.include?(coupon[:item])
+         if cart[coupon[:item]][:count] >= coupon[:num]
+           itemwithCoupon = "#{coupon[:item]} W/COUPON"
+           if cart[itemwithCoupon]
+             cart[itemwithCoupon][:count] += coupon[:num]
+             cart[coupon[:item]][:count] -= coupon[:num]
+           else
+             cart[itemwithCoupon] = {}
+             cart[itemwithCoupon][:price] = (coupon[:cost] / coupon[:num])
+             cart[itemwithCoupon][:clearance] = cart[coupon[:item]][:clearance]
+             cart[itemwithCoupon][:count] = coupon[:num]
+             cart[coupon[:item]][:count] -= coupon[:num]
+           end
         end
       end
+      if cart[coupon[:item]][:count] >= coupon[:num]
+        apply_coupons
     end
-    result[food] = info
   end
-  result
+  cart
 end
+
 
 def apply_clearance(cart)
   # code here
